fa1351941938ae1e43315bfca0c750e16f8e02a2
Paul Chicos <paulchicos@Pauls-MacBook-Pro.local>
2020-03-04 23:39:56 -0600

    AVLTree

--- Hello.py
+++ Hello.py
@@ -1,1 +1,27 @@
-print("Hello World")
+class Node:
+
+	def __init__(self, val, next = None):
+		self.val = val
+		self.next = next
+
+class List:
+
+	def __init__(self):
+		self.head = None
+
+	def add(self, value):
+		if self.head == None:
+			self.head = Node(value)
+		else:
+			self.holder = self.head
+			while self.holder.next != None:
+				self.holder = self.holder.next
+			self.holder.next = Node(value)
+
+
+
+myList = List()
+myList.add("Hello")
+myList.add("Thing")
+print(myList.head.val)
+print(myList.head.next.val)
